0f7f80bdbbf736a5e020d10be1b96c4916c998ed
Amr7moda <amrhamouda2016@gmail.com>
2022-05-30 19:43:58 +0200

    edit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,89 +1,95 @@
+@extends('web.layout')
+
+@section('title')
+create
+@endsection
+
+@section('body')
 
 
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
+<div class="container ">
+    <div class="row p-5 login">
+        <div class="col-8  p-4 shadow border login">
+            <!-- Validation Errors -->
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        </x-slot>
+            <form method="POST" enctype="multipart/form-data" action="{{ route('register') }}">
+                @csrf
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                <!-- Fname -->
+                <div>
+                    <x-label for="fname" :value="__('First Name')" />
 
-        <form method="POST" enctype="multipart/form-data" action="{{ route('register') }}">
-            @csrf
+                    <x-input id="fname" class="block mt-1 w-full form-control" type="text" name="fname" :value="old('fname')" required autofocus />
+                </div>
 
-            <!-- Fname -->
-            <div>
-                <x-label for="fname" :value="__('First Name')" />
+                <!-- Lname -->
+                <div>
+                    <x-label for="lname" :value="__('Last Name')" />
 
-                <x-input id="fname" class="block mt-1 w-full" type="text" name="fname" :value="old('fname')" required autofocus />
-            </div>
+                    <x-input id="lname" class="block mt-1 w-full form-control" type="text" name="lname" :value="old('lname')" required autofocus />
+                </div>
 
-            <!-- Lname -->
-            <div>
-                <x-label for="lname" :value="__('Last Name')" />
+                <!-- Phone -->
+                <div>
+                    <x-label for="phone" :value="__('Phone')" />
 
-                <x-input id="lname" class="block mt-1 w-full" type="text" name="lname" :value="old('lname')" required autofocus />
-            </div>
+                    <x-input id="phone" class="block mt-1 w-full form-control" type="number" name="phone" :value="old('phone')" required autofocus />
+                </div>
 
-            <!-- Phone -->
-            <div>
-                <x-label for="phone" :value="__('Phone')" />
+                <!-- Phone -->
+                <div>
+                    <x-label for="address" :value="__('Address')" />
 
-                <x-input id="phone" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required autofocus />
-            </div>
+                    <x-input id="address" class="block mt-1 w-full form-control" type="text" name="address" :value="old('address')" required autofocus />
+                </div>
 
-            <!-- Phone -->
-            <div>
-                <x-label for="address" :value="__('Address')" />
+                <!-- job -->
+                <div>
+                    <x-label for="job" :value="__('Job')" />
 
-                <x-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autofocus />
-            </div>
+                    <x-input id="job" class="block mt-1 w-full form-control" type="text" name="job" :value="old('job')" autofocus />
+                </div>
 
-            <!-- job -->
-            <div>
-                <x-label for="job" :value="__('Job')" />
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <x-label for="email" :value="__('Email')" />
 
-                <x-input id="job" class="block mt-1 w-full" type="text" name="job" :value="old('job')" autofocus />
-            </div>
+                    <x-input id="email" class="block mt-1 w-full form-control" type="email" name="email" :value="old('email')" required />
+                </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                <!-- profile picture -->
+                <div class="my-4">
+                    <label class="p-1" for="profile picture"> Choose Your Profile Picture</label>
+                    <x-input class="form-control p-2" type="file" name="pimage" id="" />
+                </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-label for="password" :value="__('Password')" />
 
-            <!-- profile picture -->
-            <div class="my-4">
-                <label class="p-1" for="profile picture"> Choose Your Profile Picture</label>
-                <x-input class="form-control p-2" type="file" name="pimage" id="" />
-            </div>
+                    <x-input id="password" class="block mt-1 w-full form-control" type="password" name="password" required autocomplete="new-password" />
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <!-- Confirm Password -->
+                <div class="mt-4">
+                    <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                    <x-input id="password_confirmation" class="block mt-1 w-full form-control" type="password" name="password_confirmation" required />
+                </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                <div class="flex items-center justify-end mt-4">
+                    <a class="text-decoration-none mt-2 pe-3" href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
+                    </a>
 
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required />
-            </div>
+                    <x-button class="btn btn-lg p-2 shadow w-25" style="color: black !important;">
+                        {{ __('Register') }}
+                    </x-button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
-
+@endsection
